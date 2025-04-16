6c80b0d4d33208926c6e2251090c195c93b2e704
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-04-16 23:30:34 +0300

    Added Project Slider

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,5 +1,22 @@
+import {ProjectSlider} from '@/components/ProjectSlider'
+
 const page = () => {
-	return <div>My Projects</div>
+	return (
+		<div className='flex items-center justify-center gap-5 flex-col md:flex-row h-screen bg-[#0C011A]'>
+			<div className='flex flex-col gap-3'>
+				<h1 className='text-[50px] text-white font-semibold'>
+					My Work <span className='text-red-500'>.</span>
+				</h1>
+				<p className='max-w-[400px] text-[16px] text-gray-200 md:text-gray-400'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+					dicta beatae delectus officia minima fugiat animi repudiandae ad
+					aperiam. Earum tempora asperiores quod harum mollitia? Asperiores
+					aperiam quod blanditiis. Voluptate.
+				</p>
+			</div>
+			<ProjectSlider/>
+		</div>
+	)
 }
 
 export default page
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const Navbar = () => {
 	return (
-		<div className='fixed top-0 bg-transparent z-[20] w-full flex gap-5 md:justify-between px-60 p-5'>
+		<div className='fixed top-0 bg-transparent z-[20] w-full flex gap-5 md:justify-between md:px-60 p-5'>
 			<h1 className='text-white text-[45px]'>
 				Astemir <span className='font-thin'>Gurizhev</span>
 				<span className='text-red-500'>.</span>
